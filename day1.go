cfc6ca84e80558c8604f446c8d24c45845f1a231
Ben Blankley <ben@localhost.localdomain>
2015-12-03 11:14:28 -0600

    ran gofmt

--- day1.go
+++ day1.go
@@ -1,20 +1,20 @@
- package main
+package main
 
- import (
-         "fmt"
-         "os"
-         "strings"
- )
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
- func main() {
-         file, err := os.Open("./day1input.txt")
-		floor := 0
-         if err != nil {
-                 fmt.Println(err)
-                 os.Exit(1)
-         }
+func main() {
+	file, err := os.Open("./day1input.txt")
+	floor := 0
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-         defer file.Close()
+	defer file.Close()
 
 	for {
 		data := make([]byte, 1)
@@ -23,14 +23,14 @@
 			break
 		}
 		fmt.Printf("read %d byte: %q\n", count, data[:count])
-		if strings.EqualFold(string(data[:count]), "(") {  
+		if strings.EqualFold(string(data[:count]), "(") {
 			floor = floor + 1
 		} else {
 			floor = floor - 1
 		}
-		
+
 	}
-		fmt.Printf("Floor: %d\n", floor)
-// }
-		
- }
+	fmt.Printf("Floor: %d\n", floor)
+	// }
+
+}
