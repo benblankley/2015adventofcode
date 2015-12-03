589bf120f6e35a9ca82da808aa2ff930ad0d12e2
Ben Blankley <ben.blankley@gmail.com>
2015-12-03 06:50:31 +0000

    made program but doesn't compile

---
+++ day1.go
@@ -0,0 +1,31 @@
+ package main
+
+ import (
+         "fmt"
+         "os"
+         "strings"
+ )
+
+ func main() {
+         file, err := os.Open("./day1input.txt")
+		floor := 0
+         if err != nil {
+                 fmt.Println(err)
+                 os.Exit(1)
+         }
+
+         defer file.Close()
+
+// if { // Big loop to iterate through file
+		data := make([]byte, 1)
+		count, err := file.Read(data)
+		fmt.Printf("read %d byte: %q\n", count, data[:count])
+		if strings.EqualFold(data[:count], "(") {  // this line doesn't work. bytes can't be compared with strings
+			floor = floor + 1
+		} else {
+			floor = floor - 1
+		}
+		fmt.Printf("Floor: %q\n", floor)
+// }
+		
+ }
