614b2320a395ba3c0e0c491584d27a8038d49e7c
Ben Blankley <ben.blankley@gmail.com>
2015-12-10 04:47:13 +0000

    Added string-to-integer code

--- day2.go
+++ day2.go
@@ -4,6 +4,8 @@ import (
     "bufio"
     "fmt"
     "os"
+    "strings"
+    "strconv"
 )
 
 func main() {
@@ -18,7 +20,14 @@ func main() {
 	scanner := bufio.NewScanner(file)
     for scanner.Scan() {
         line := scanner.Text()
-        fmt.Printf("line is %[1]T %[1]q\n", line)
+        fmt.Printf("line is %[1]T %[1]q ", line)
+        coordinates := strings.Split(line, "x")
+
+        l, _ := strconv.Atoi(coordinates[0])
+        w, _ := strconv.Atoi(coordinates[1])
+        h, _ := strconv.Atoi(coordinates[2])
+        fmt.Printf("%d %d %d\n", l, w, h)
+        
     }
 	
 }
