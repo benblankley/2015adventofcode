e9b270db96cd867fc193916961646222205aec97
Ben Blankley <ben@localhost.localdomain>
2015-12-10 00:24:00 -0600

    added lots of debug output:

--- day2.go
+++ day2.go
@@ -17,6 +17,7 @@ func main() {
 
 	defer file.Close()
 	
+	area := 0
 	scanner := bufio.NewScanner(file)
     for scanner.Scan() {
         line := scanner.Text()
@@ -26,8 +27,28 @@ func main() {
         l, _ := strconv.Atoi(coordinates[0])
         w, _ := strconv.Atoi(coordinates[1])
         h, _ := strconv.Atoi(coordinates[2])
-        fmt.Printf("%d %d %d\n", l, w, h)
-        
+        fmt.Printf("%d %d %d  cumulative area:%d ", l, w, h, area)
+	
+	if l*w < w*h && l*w < h*l {
+		areanew := (2*l*w + 2*w*h + 2*h*l)
+		areaextra := l*w
+        fmt.Printf("l*w is smallest\n")
+        fmt.Printf("%d %d\n", areanew, areaextra)
+	area = area + areanew + areaextra        
+	} else if w*h < l*w && w*h < h*l {
+		areanew := (2*l*w + 2*w*h + 2*h*l)
+		areaextra := w*h
+        fmt.Printf("w*h is smallest\n")
+        fmt.Printf("%d %d\n", areanew, areaextra)
+	area = area + areanew + areaextra        
+	} else {
+		areanew := (2*l*w + 2*w*h + 2*h*l)
+		areaextra := h*l
+        fmt.Printf("h*l is smallest\n")
+        fmt.Printf("%d %d\n", areanew, areaextra)
+	area = area + areanew + areaextra        
+	}
     }
+        fmt.Printf("Area:  %d\n", area)
 	
 }
