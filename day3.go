459c6cdde1d5f9f38dc5b8bc671b727bd21ca5eb
Ben Blankley <ben.blankley@gmail.com>
2015-12-11 11:11:37 -0600

    Trying some struct and bool types

--- day3.go
+++ day3.go
@@ -3,39 +3,37 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+	"io/ioutil"
 )
 
 func main() {
-	file, err := os.Open("./day3input.txt")
-	santax := 0
-	santay := 0
-	
-	var Presents [100][100]int
+	file, err := ioutil.ReadFile("day3input.txt"])
+
+	// create new struct type called "house", which is basically an array of x and y coordinates
+	type house struct {
+		x, y int
+	}
 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	defer file.Close()
-
-	for {
-		data := make([]byte, 1)
-		_, err := file.Read(data)
-		if err != nil {
-			break
-		}
-		if strings.EqualFold(string(data[0]), ">") {
-			santax++
-		} else if strings.EqualFold(string(data[0]), "<") {
-			santax--
-		} else if strings.EqualFold(string(data[0]), "^") {
-			santay++
-		} else if strings.EqualFold(string(data[0]), "v") {
-			santay--
-		}
-		fmt.Printf("X: %10d Y: %10d \n", santax, santay)
-		Presents[santax][santay] =+ 1
-	}
+	visitedhouses := make(map[house]bool)
+	
+ 		var santa house 
+  		for _, count := range input { 
+ 			switch count { 
+ 			case '<': 
+ 				santa.x-- 
+ 			case 'v': 
+ 				santa.y++ 
+ 			case '>': 
+ 				santa.x++ 
+ 			case '^': 
+ 				santa.y-- 
+ 			} 
+ 		visitedhouses[*santa] = true
+		} 
+		
+	fmt.Printf("Houses by just santa: %d", len(visitedhouses))
 }
