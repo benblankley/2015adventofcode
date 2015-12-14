f6b93375c86a596c3259f12a281c3fb51f79ec8e
Ben Blankley <ben.blankley@gmail.com>
2015-12-13 20:02:40 -0600

    Added five and six zeroes options, added appending to output file

--- day4.sh
+++ day4.sh
@@ -6,22 +6,30 @@ echo "Advent of Code, 2015: Day 4"
 INPUTCODE=ckczppom
 
 echo "Starting calculation..."
-for COUNTER in $(seq 1 1000000)
+for COUNTER in $(seq 1 10000000)
   do
   # bash
   # sum=$(md5sum <<< $COUNTER$INPUTCODE | cut -d ' ' -f 1)
   # dash
   sum=$(echo $COUNTER$INPUTCODE | md5sum)
 
-  cutsum=$(echo $sum | cut -c1-2)
+  cutsum=$(echo $sum | cut -c1-5)
+  cutsum2=$(echo $sum | cut -c1-6)
 
   # Output debugging information
   # echo "Input: " $COUNTER$INPUTCODE " | "  "Santa MD5: " $sum " | "  "Cut Santa MD5: " $cutsum
 
-  if [ $cutsum = "00" ]
+  if [ $cutsum = "00000" ]
   then
-    echo "Lowest Value with two zeroes: " $COUNTER  "  MD5: " $sum
-    echo "Lowest Value with two zeroes: " $COUNTER  "  MD5: " $sum > output.txt
+    echo "Lowest Value with five zeroes: " $COUNTER  "  MD5: " $sum
+    echo "Lowest Value with five zeroes: " $COUNTER  "  MD5: " $sum >> output.txt
+    break
+  fi
+
+  if [ $cutsum2 = "000000" ]
+  then
+    echo "Lowest Value with six zeroes: " $COUNTER  "  MD5: " $sum
+    echo "Lowest Value with six zeroes: " $COUNTER  "  MD5: " $sum >> output.txt
     break
   fi
 
