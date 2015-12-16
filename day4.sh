22006601a0ffd0b0c9a26a4d076e389ac952e908
Ben Blankley <ben.blankley@gmail.com>
2015-12-15 22:30:43 -0600

    Updated day4.sh

--- day4.sh
+++ day4.sh
@@ -16,23 +16,24 @@ for COUNTER in $(seq 1 10000000)
   cutsum2=$(echo -n "$sum" | cut -c1-6)
 
   # Output debugging information
-  # echo "Input: " $COUNTER$INPUTCODE " | "  "Santa MD5: " $sum " | "  "Cut Santa MD5: " $cutsum
+  echo "Input: " $COUNTER$INPUTCODE " | "  "Santa MD5: " $sum " | "  "Cut Santa MD5: " $cutsum
 
   # Output minimal debug information
-  if [ (COUNTER % 1000) -eq 0 ]
+  if [ $((COUNTER % 1000)) -eq 0 ]
   then
     echo "Step: " "$COUNTER"
+    echo "Step: " "$COUNTER" >> status.txt
   fi
 
 
-  if [ "$cutsum" = "00000" ]
+  if [ "$cutsum"="00000" ]
   then
     echo "Lowest Value with five zeroes: " "$COUNTER"  "  MD5: " "$sum"
     echo "Lowest Value with five zeroes: " "$COUNTER"  "  MD5: " "$sum" >> output.txt
     break
   fi
 
-  if [ "$cutsum2" = "000000" ]
+  if [ "$cutsum2"="000000" ]
   then
     echo "Lowest Value with six zeroes: " "$COUNTER"  "  MD5: " "$sum"
     echo "Lowest Value with six zeroes: " "$COUNTER"  "  MD5: " "$sum" >> output.txt
