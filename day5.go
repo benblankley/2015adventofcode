3c41e617fa0d6374c61f10e94720d46c335e5331
Ben Blankley <ben.blankley@gmail.com>
2015-12-17 11:15:32 -0600

    Update day5.go

--- day5.go
+++ day5.go
@@ -72,7 +72,7 @@ func main() {
 			isnice[line] = true			
 		}
 //		Check for double letter combinations
-		fmt.Printf("line: %q contains aa: %t /n", line, strings.Contains(line, "aa"))
+		fmt.Println("line contains aa", line, strings.Contains(line, "aa"))
 
 //		Check for special cases
 		if (strings.Contains(line, "ab") || strings.Contains(line, "cd") || strings.Contains(line, "pq") || strings.Contains(line, "xy")) {
