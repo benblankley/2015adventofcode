8f04deb97fb6e9cc6103b55041a81758ae00d459
Ben Blankley <ben.blankley@gmail.com>
2015-12-20 23:29:03 -0600

    Added Part 2 Solution

--- day6.go
+++ day6.go
@@ -37,6 +37,36 @@ import (
 // 
 // After following the instructions, how many lights are lit?
 
+// --- Part Two ---
+// 
+// You just finish implementing your winning light pattern when you realize
+// you mistranslated Santa's message from Ancient Nordic Elvish.
+//
+// The light grid you bought actually has individual brightness controls; each 
+// light can have a brightness of zero or more. The lights all start at zero.
+//
+// The phrase turn on actually means that you should increase the brightness 
+// of those lights by 1.
+//
+// The phrase turn off actually means that you should decrease the brightness 
+// of those lights by 1, to a minimum of zero.
+//
+// The phrase toggle actually means that you should increase the brightness of 
+// those lights by 2.
+//
+// What is the total brightness of all lights combined after following Santa's 
+// instructions?
+//
+// For example:
+// 
+//  - turn on 0,0 through 0,0 would increase the total brightness by 1.
+//  - toggle 0,0 through 999,999 would increase the total brightness by 
+//    2000000.
+
+
+
+
+
 func main() {
 	file, err := os.Open("./day6input.txt")
 	if err != nil {
@@ -49,6 +79,7 @@ func main() {
 	fmt.Println("--- Day 6: Probably a Fire Hazard ---")
 
 	lights := [1000][1000]bool{}
+	lights_p2 := [1000][1000]int{}
 
 	scanner := bufio.NewScanner(file)
 	x_start := 0
@@ -72,9 +103,11 @@ func main() {
 			s2 := strings.Split(splitline[3], ",")
 			x_end, _ = strconv.Atoi(s2[0])
 			y_end, _ = strconv.Atoi(s2[1])
-			for i := x_start; i < x_end; i++ {
-				for j := y_start; j < y_end; j++ {
+			for i := x_start; i <= x_end; i++ {
+				for j := y_start; j <= y_end; j++ {
 				    lights[i][j] = !lights[i][j]
+			            // Part 2
+				    lights_p2[i][j] = lights_p2[i][j] + 2
 				}
 			}
 		} else if splitline[0] == "turn" && splitline[1] == "on" {
@@ -85,9 +118,11 @@ func main() {
 			s2 := strings.Split(splitline[4], ",")
 			x_end, _ = strconv.Atoi(s2[0])
 			y_end, _ = strconv.Atoi(s2[1])
-                        for i := x_start; i < x_end; i++ {
-                                for j := y_start; j < y_end; j++ {
+                        for i := x_start; i <= x_end; i++ {
+                                for j := y_start; j <= y_end; j++ {
                                     lights[i][j] = true
+			            // Part 2
+				    lights_p2[i][j]++
                                 }
                         }
 		} else if splitline[0] == "turn" && splitline[1] == "off" {
@@ -98,25 +133,40 @@ func main() {
 			s2 := strings.Split(splitline[4], ",")
                         x_end, _ = strconv.Atoi(s2[0])
                         y_end, _ = strconv.Atoi(s2[1])
-                        for i := x_start; i < x_end; i++ {
-                                for j := y_start; j < y_end; j++ {
+                        for i := x_start; i <= x_end; i++ {
+                                for j := y_start; j <= y_end; j++ {
                                     lights[i][j] = false
+			            // Part 2
+				    lights_p2[i][j]--
+				    if lights_p2[i][j] < 0 { lights_p2[i][j] = 0 }
                                 }
                         }
 		}
-		// fmt.Printf("%d\n", instruction)
-		fmt.Printf("x_start %d\n", x_start)
-		fmt.Printf("y_start %d\n", y_start)
-		fmt.Printf("x_end %d\n", x_end)
-		fmt.Printf("y_end %d\n", y_end)
+//		fmt.Printf("x_start %d\n", x_start)
+//		fmt.Printf("y_start %d\n", y_start)
+//		fmt.Printf("x_end %d\n", x_end)
+//		fmt.Printf("y_end %d\n", y_end)
 
 	}
 	lightcount := 0
-        for i := 0; i < 1000; i++ {
-        	for j := 0; j < 1000; j++ {
+	brightness :=0
+        for i := 0; i <= 999; i++ {
+        	for j := 0; j <= 999; j++ {
                      if lights[i][j] { lightcount++ }
+		     brightness = brightness + lights_p2[i][j]
                 }
       	}
 
 	fmt.Println("Number of lights on: ", lightcount)
+	fmt.Println("Brightness: ", brightness)
+
+	counter := 0
+	for i := 0; i <= 999; i++ {
+                for j := 0; j <= 999; j++ {
+                	counter++
+        	}
+        }
+
+	fmt.Println("Counter: ", counter)
+
 }
